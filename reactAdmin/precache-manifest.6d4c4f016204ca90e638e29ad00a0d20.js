414b009d888b52c5fdc269a12d091aef86acd474
xypecho <xypecho@163.com>
2019-04-15 22:16:24 +0800

    Site updated: 2019-04-15 22:16:05

---
+++ reactAdmin/precache-manifest.6d4c4f016204ca90e638e29ad00a0d20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6207d507353a29070939",
+    "url": "/static/js/main.34ddc2d0.chunk.js"
+  },
+  {
+    "revision": "0fba0a4f6fc4a8a45232",
+    "url": "/static/js/2.8e767acc.chunk.js"
+  },
+  {
+    "revision": "6207d507353a29070939",
+    "url": "/static/css/main.243c31c4.chunk.css"
+  },
+  {
+    "revision": "0fba0a4f6fc4a8a45232",
+    "url": "/static/css/2.bbd2085c.chunk.css"
+  },
+  {
+    "revision": "8625058cd74d73e8127edf6655733ed2",
+    "url": "/index.html"
+  }
+];
